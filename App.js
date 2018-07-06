6b687a50e85289785ecbba38e3af5acf419a7a1f
Dan Bednar <dbednar13@gmail.com>
2018-07-05 19:23:13 -0500

    Adding components

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text> HI Michelle </Text>
+        <Text> HI Michelle test after refresh </Text>
       </View>
     );
   }
