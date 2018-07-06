e0b2ad145a6c4e290d5331689820c73ee59abe88
Dan Bednar <dbednar13@gmail.com>
2018-07-05 19:32:09 -0500

    Add Entry working
    Note, will need to readd stuff that was taken out to make it all run properly.

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text> HI Michelle test after refresh </Text>
+        <Text> HI Michelle test after refresh </Text> */}
+        <AddEntry />
       </View>
     );
   }
