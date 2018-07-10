9751cefb0c715c1a519a7947ee1538a4bacb0324
Dan Bednar <dbednar13@gmail.com>
2018-07-09 20:02:31 -0500

    Adding functionality to sliders and steppers

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +16,27 @@ export default class App extends React.Component {
       <View>
         <AddEntry />
       </View>
-    )
+    );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff',
+  },
+});
