384cb1b126ac45cdb8e6ddb289d081416eebf166
Dan Bednar <dbednar13@gmail.com>
2018-07-09 21:03:29 -0500

    Store Created

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from '../utils/api';
 
 function SubmitBtn({ onPress }) {
   return (
@@ -60,7 +61,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    submitEntry(key, entry);
 
     // Clear local notification
   };
@@ -70,7 +71,7 @@ export default class AddEntry extends Component {
 
     // Navigate to home
 
-    // Save to "DB"
+    removeEntry(key);
 
     // Clear local notification
   };
